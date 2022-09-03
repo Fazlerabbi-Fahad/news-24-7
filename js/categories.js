1ca56a7aafb8df9566a570f47cede0e2b8d71778
Fazle Rabbi Fahad <fazlerabbifahad.54@gmail.com>
2022-09-03 13:25:13 +0600

    Header Section

--- js/categories.js
+++ js/categories.js
@@ -1,7 +1,18 @@
 const loadCategories = () => {
     fetch('https://openapi.programming-hero.com/api/news/categories')
         .then(res => res.json())
-        .then(data => console.log(data.data.news_category))
+        .then(data => displayCategories(data.data.news_category))
 }
 
+const displayCategories = (newses) => {
+    const categorySection = document.getElementById('category');
+    for (const news of newses) {
+        const categoryLi = document.createElement('li');
+        categoryLi.classList.add('nav-item');
+        categoryLi.innerHTML = `
+        <a class="nav-link category" aria-current="page" href="#">${news.category_name}</a>
+        `
+        categorySection.appendChild(categoryLi);
+    }
+}
 loadCategories()
