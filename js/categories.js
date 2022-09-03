ec548783097493d2fff83f5902b24ce386888012
Fazle Rabbi Fahad <fazlerabbifahad.54@gmail.com>
2022-09-04 00:37:47 +0600

    Box Page

--- js/categories.js
+++ js/categories.js
@@ -5,14 +5,30 @@ const loadCategories = () => {
 }
 
 const displayCategories = (newses) => {
+    toggleSpinner(true);
     const categorySection = document.getElementById('category');
     for (const news of newses) {
         const categoryLi = document.createElement('li');
         categoryLi.classList.add('nav-item');
         categoryLi.innerHTML = `
-        <a class="nav-link category" aria-current="page" href="#">${news.category_name}</a>
+        <a class="nav-link category" onclick="loadCategory(${news.category_id})"aria-current="page" href="#">${news.category_name}</a>
         `
         categorySection.appendChild(categoryLi);
+
     }
+    toggleSpinner(false);
 }
 loadCategories()
+
+
+const toggleSpinner = isLoading => {
+    const loaderSection = document.getElementById('loader');
+    if (isLoading) {
+        loaderSection.classList.remove('d-none');
+    } else {
+        loaderSection.classList.add('d-none');
+    }
+}
+
+
+
